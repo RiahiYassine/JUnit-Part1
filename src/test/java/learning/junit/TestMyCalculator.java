8537ea7e6f04d690232c8b49f5b23e3a05c18537
yassineRiahi <yassine.riahi@marketingconfort.com>
2025-04-07 15:58:50 +0100

    "test : Add JUnit test for MyCalculator substract method"

--- src/test/java/learning/junit/TestMyCalculator.java
+++ src/test/java/learning/junit/TestMyCalculator.java
@@ -21,6 +21,10 @@ class TestMyCalculator {
         assertNotEquals(5 , myCalculator.add(1,3), "Expected 1 + 3 not to equal 5");
     }
 
-
+    @Test
+    void testSubstract(){
+        assertEquals(1 , myCalculator.subtract(3,2), "Expected 3 - 2 to equal 1");
+        assertNotEquals(6 , myCalculator.subtract(3,2), "Expected 3 - 2 not to equal 6");
+    }
 
 }
