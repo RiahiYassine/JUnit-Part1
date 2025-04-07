8f84452d3b54501366a0aadacb168c928b8e1675
yassineRiahi <yassine.riahi@marketingconfort.com>
2025-04-07 15:56:05 +0100

    "feat : add basic MyCalculator class with add and substract methods"

---
+++ src/main/java/learning/junit/MyCalculator.java
@@ -0,0 +1,14 @@
+package learning.junit;
+
+public class MyCalculator {
+
+    public int add ( int a , int b){
+        return a+b;
+    }
+
+    public int subtract ( int a , int b){
+        return a-b;
+    }
+
+
+}
