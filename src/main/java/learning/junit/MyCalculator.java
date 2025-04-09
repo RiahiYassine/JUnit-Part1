6b63f685dee8f5c2da526796566227929c63e9ba
yassineRiahi <171589312+RiahiYassine@users.noreply.github.com>
2025-04-09 20:11:58 +0100

    Remove subtract method and test

--- src/main/java/learning/junit/MyCalculator.java
+++ src/main/java/learning/junit/MyCalculator.java
@@ -5,10 +5,4 @@ public class MyCalculator {
     public int add ( int a , int b){
         return a+b;
     }
-
-    public int subtract ( int a , int b){
-        return a-b;
-    }
-
-
 }
--- src/test/java/learning/junit/TestMyCalculator.java
+++ src/test/java/learning/junit/TestMyCalculator.java
@@ -21,10 +21,4 @@ class TestMyCalculator {
         assertNotEquals(5 , myCalculator.add(1,3), "Expected 1 + 3 not to equal 5");
     }
 
-    @Test
-    void testSubtract(){
-        assertEquals(1 , myCalculator.subtract(3,2), "Expected 3 - 2 to equal 1");
-        assertNotEquals(6 , myCalculator.subtract(3,2), "Expected 3 - 2 not to equal 6");
-    }
-
 }
